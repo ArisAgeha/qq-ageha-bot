86673021c90ea3fe8bf16bdd9a03aa733027214d
ArisAgeha <87725092@qq.com>
2020-06-29 04:01:54 +0800

    MoeWiki - intro && section

--- src/receiver/MoeWiki.ts
+++ src/receiver/MoeWiki.ts
@@ -1,10 +1,12 @@
 import { App, Meta } from "koishi";
 import axios from 'axios';
+import { isObject } from "util";
 
 export class MoeWiki {
 
     app: App;
-    prefixs: ['moe', '萌百'];
+    prefixs = ['moe', 'moe ', '萌百', '萌百 '];
+    userData = {};
 
     constructor(app: App) {
         this.app = app;
@@ -20,25 +22,164 @@ export class MoeWiki {
             this.prefixs.forEach(word => {
                 if (text.toLowerCase().startsWith(word + ' ')) this.startSearch(msg);
             });
+
+            if (/^\d+$/.test(text)) this.getSection(msg);
         })
     }
 
     private async startSearch(msg: Meta<'message'>) {
+        const sender = msg.sender.userId;
         const word = this.getWord(msg.rawMessage);
-        
-        const html = await axios.get(`https://zh.moegirl.org/${word}`);
-        console.log(html);
+        const $ = await this.fetchHtml(word, msg);
+        if (!$) return;
+        const vNode = this.buildVNode($);
+        this.saveVNode(msg, vNode);
+
+        const res = vNode.introduction + '\r\n' + vNode.catalog + '\r\n' + vNode.suffix;
+        msg.$send(`[CQ:at,qq=${sender}]\r\n${res}`);
     }
 
-    private getWord(text: string) {
+    private getSection(msg: Meta<'message'>) {
+        const sender = msg.sender.userId;
+        if (!this.userData[sender]) return;
+
+        const { vNode, requestTime } = this.userData[sender];
+        const now = Date.now();
+        if (now - requestTime > 300 * 1000) return;
+
+        const index = msg.rawMessage;
+        const section = vNode.section[index];
+        msg.$send(`[CQ:at,qq=${sender}]\r\n${section}`);
+    }
+
+    private buildVNode($: any): VNode {
+        let content = $('.mw-parser-output');
+        content = content[1] ? content[1] : content[0];
+
+        const catalog: string[] = this.buildCatalog($);
+        const catalogString: string = this.buildCatalogString(catalog);
+        const introduction: string = this.buildIntroduction($, content);
+        const suffix = '5分钟内，输入词条目录的编号可获取词条内容';
+        const section = this.buildSection(content, catalog);
+
+        return {
+            catalog: catalogString,
+            introduction,
+            suffix,
+            section
+        }
+    }
+
+    private saveVNode(msg: Meta<'message'>, vNode: VNode) {
+        const sender = msg.sender.userId;
+        this.userData[sender] = { requestTime: Date.now(), vNode: vNode }
+    }
+
+    private buildSection(content: any, catalog: string[]) {
+        const domArray = content.children;
+        const sections = [];
+        let flags = '';
+        let counter = 0;
+        for (let i = 0; i < domArray.length; i++) {
+            const item = domArray[i];
+            if (item.name === 'h2') {
+                counter++;
+                const title = item.children[1]?.children[0]?.data;
+                flags = title;
+                sections[counter] = '';
+            }
+            else {
+                const text = this.recursedTag(item);
+                sections[counter] += text;
+            }
+        }
+
+        return sections;
+    }
+
+    private buildCatalog($: any) {
+        const catalog = [];
+        const dirHtml = $('#toc .toclevel-1 > a');
+        const dirHtmlArray = dirHtml[0] ? Object.values(dirHtml) : [dirHtml];
+        dirHtmlArray.forEach((item: any) => {
+            if (item?.attribs?.href) catalog.push(item.attribs.href.slice(1));
+        });
+        return catalog;
+    }
+
+    private buildCatalogString(catalog: string[]) {
+        let catalogInfo = '词条目录：\r\n'
+        catalogInfo += catalog.reduce((prev, cur, cin) => {
+            return `${prev}\r\n${cin + 1}：${cur}`;
+        }, '');
+        return catalogInfo;
+    }
+
+    private buildIntroduction($: any, content: any) {
+        let introduction = '';
+        for (let i = 0; i < content.children.length; i++) {
+            const item = content.children[i];
+            if (item.name === 'p') {
+                if (item.children) {
+                    const text: string = this.recursedTag(item);
+                    if (text) introduction += text;
+                }
+            }
+
+            if (item.name === 'h2') break;
+        }
+        return introduction;
+    }
+
+    private recursedTag(item: any): string {
+        let intro = '';
+        const paraArray = item.children;
+        if (!paraArray) return;
+        paraArray.forEach(para => {
+            if (['style', 'script'].includes(para.type)) return;
+            if (para.data) intro += para.data;
+            if (para.children) {
+                intro += this.recursedTag(para);
+            }
+
+        });
+        return intro;
+    }
+
+    private async fetchHtml(word: string, msg: Meta<'message'>) {
+        let res;
+        try {
+            res = await axios.get(encodeURI(`https://zh.moegirl.org/${word}`));
+            console.log('fetch success');
+            const html = res.data;
+            const cheerio = require('cheerio');
+            const $ = cheerio.load(html);
+            return $;
+        }
+        catch (err) {
+            console.log(err);
+        }
+
+    }
+
+    private getWord = (text: string) => {
         let word = '';
 
         for (let prefixWord of this.prefixs) {
-            if (text.toLowerCase().startsWith(word + ' ')) {
-                const slicePos = prefixWord.length + 1;
+            if (text.toLowerCase().startsWith(word)) {
+                const slicePos = prefixWord.length;
                 word = text.slice(slicePos);
                 break;
             }
         }
+
+        return word;
     }
+}
+
+type VNode = {
+    catalog: string;
+    introduction: string;
+    suffix: string;
+    section: string[]
 }

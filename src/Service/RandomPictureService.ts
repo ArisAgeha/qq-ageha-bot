cb445559de1468b1946753f5fae9ccffb9fd5f40
ArisAgeha <87725092@qq.com>
2020-07-27 19:48:49 +0800

    RandomPicture: seperate code to RandomPictureService

--- src/Service/RandomPictureService.ts
+++ src/Service/RandomPictureService.ts
@@ -1,3 +1,50 @@
+import puppeteer = require('puppeteer');
+import fs = require('fs');
+import path = require('path');
+
 export class RandomPictureService {
-    
+    async getPicture(keyword) {
+        try {
+            const src = `https://image.baidu.com/search/index?tn=baiduimage&ps=1&ct=201326592&lm=-1&cl=2&nc=1&ie=utf-8&word=${keyword}`;
+
+            const browser = await puppeteer.launch({});
+            const page = await browser.newPage();
+            await page.goto(src);
+            page.setViewport({
+                width: 1600,
+                height: 5000
+            });
+
+            await page.waitFor(2000);
+
+            const els = await page.$$('.main_img.img-hover');
+            const el = els[Math.floor(Math.random() * els.length)];
+            let imgs = await page.$$eval('.main_img.img-hover', imgs => imgs.map(img => img.getAttribute('src'))) as any;
+            imgs = imgs.filter(img => {
+                return img.startsWith('https');
+            });
+            const img = imgs[Math.floor(Math.random() * imgs.length)];
+
+            await page.goto(img);
+            const detailPageEl = await page.$('img');
+
+            const rd = String(Math.ceil(Math.random() * 10000000));
+            const tempPath = path.resolve(`./temp/${rd}.png`);
+            const savePath = path.resolve(`I:\\酷Q Pro\\data\\image\\${rd}.png`);
+
+            await detailPageEl.screenshot({ path: tempPath });
+
+            fs.renameSync(tempPath, savePath);
+
+            setTimeout(() => {
+                fs.unlink(savePath, () => { });
+            }, 30 * 1000);
+
+            return rd;
+        }
+        catch (err) {
+            console.error(err);
+            return false;
+        }
+    }
 }
--- src/receiver/RandomPicture.ts
+++ src/receiver/RandomPicture.ts
@@ -11,7 +11,6 @@ export class RandomPicture {
 
     app: App;
     prefix = '随机';
-    setPrefix = '随机 -set';
     randomPictureService: RandomPictureService;
 
     constructor(app: App) {
@@ -26,93 +25,18 @@ export class RandomPicture {
         this.app.receiver.on('message', (msg) => {
             const text = msg.rawMessage;
 
-            if (text.toLowerCase().startsWith(this.setPrefix)) this.setSize(msg);
-            else if (text.toLowerCase().startsWith(this.prefix)) this.getRandomPicture(msg);
+            if (text.toLowerCase().startsWith(this.prefix)) this.getRandomPicture(msg);
         })
-    }
-
-    private async setSize(msg: Meta<'message'>) {
-
     }
 
     private async getRandomPicture(msg: Meta<'message'>) {
-        try {
-            const text = msg.rawMessage;
-            const obj = text.slice(2);
-            msg.$send(`${obj}加载中...`);
-            const src = `https://image.baidu.com/search/index?tn=baiduimage&ps=1&ct=201326592&lm=-1&cl=2&nc=1&ie=utf-8&word=${obj}`;
+        const text = msg.rawMessage;
+        const keyword = text.slice(2);
+        msg.$send(`${keyword}加载中...`);
 
-            const browser = await puppeteer.launch({});
-            const page = await browser.newPage();
-            await page.goto(src);
-            page.setViewport({
-                width: 1600,
-                height: 5000
-            });
+        const rd = await this.randomPictureService.getPicture(keyword);
 
-            await this.autoScroll(page);
-            await page.waitFor(3000);
-
-            const els = await page.$$('.main_img.img-hover');
-            const el = els[Math.floor(Math.random() * els.length)];
-            let imgs = await page.$$eval('.main_img.img-hover', imgs => imgs.map(img => img.getAttribute('src'))) as any;
-            imgs = imgs.filter(img => {
-                return img.startsWith('https');
-            });
-            const img = imgs[Math.floor(Math.random() * imgs.length)];
-
-            await page.goto(img);
-            const detailPageEl = await page.$('img');
-
-            const rd = String(Math.ceil(Math.random() * 10000000));
-            const tempPath = path.resolve(`./temp/${rd}.png`);
-            const savePath = path.resolve(`I:\\酷Q Pro\\data\\image\\${rd}.png`);
-
-            await detailPageEl.screenshot({ path: tempPath });
-
-            fs.rename(tempPath, savePath, async (err) => {
-                console.log(err);
-                if (!err) {
-                    const pictureCq = `[CQ:image,file=${rd}.png]`;
-                    await msg.$send(pictureCq);
-
-                    setTimeout(() => {
-                        fs.unlink(savePath, () => { });
-                    }, 300 * 1000);
-                }
-            });
-        }
-        catch (err) {
-            console.error(err);
-            msg.$send('获取网页截屏失败，请检查你的url');
-        }
-    }
-
-    private async cropImage(targetPath, savePath, width, height, x, y) {
-        return new Promise((res, rej) => {
-            gm(targetPath).crop(width, height, x, y).write(savePath, (err) => {
-                if (err) console.log(err);
-                if (err) rej();
-                else res();
-            });
-        })
-    }
-
-    private async autoScroll(page) {
-        await page.evaluate(`(async () => {
-            await new Promise((resolve, reject) => {
-                var totalHeight = 0;
-                var distance = 400;
-                var timer = setInterval(() => {
-                    window.scrollBy(0, distance);
-                    totalHeight += distance;
-
-                    if (totalHeight >= 1200) {
-                        clearInterval(timer);
-                        resolve();
-                    }
-                }, 25);
-            });
-        })()`);
+        if (rd) msg.$send(`[CQ:image,file=${rd}.png]`);
+        else msg.$send(`没有找到内容！`);
     }
 }
